d0a933624bc6c57008a6db19b87d82dbb537df3c
brilliancem2bs4 <brilliancem2bs4@gmail.com>
2026-01-19 00:43:02 +0300

    Update cache name and simplify event listeners

--- sw.js
+++ sw.js
@@ -1,41 +1,19 @@
-const CACHE_NAME = 'adpay-syria-v1';
+const CACHE_NAME = 'adpay-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/form.html',
-  '/whatsapp.html',
-  'https://img.icons8.com/color/96/000000/syria.png'
+  '/index.html'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then(response => response || fetch(event.request))
   );
 });
