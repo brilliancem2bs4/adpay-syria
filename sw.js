bde1864f020bb68dbe429320816b7a6e7650f4c4
brilliancem2bs4 <brilliancem2bs4@gmail.com>
2026-01-18 22:21:55 +0300

    Add service worker for caching and fetching resources

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'adpay-syria-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/form.html',
+  '/whatsapp.html',
+  'https://img.icons8.com/color/96/000000/syria.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Update Service Worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
